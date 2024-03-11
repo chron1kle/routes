aa0f87e38dbb58ca5fda560b18a07392a59295aa
chron1kle <saundershwu@gmail.com>
2024-03-11 22:29:02 +0800

    S

--- dataproc.py
+++ dataproc.py
@@ -2,8 +2,21 @@ import SQL_example as sqllib
 import json, os, time
 ##  UTC + 8
 
-d612_20240101 = [(1418, 1427), (1555, 1605), (1852, 1858), (1903, 1907), (1914, 1949), (1957, 2032), (2037, 2041), (2047, 2053), (2133, 2201), (2206, 2233)]
-d612_20240102 = [(1916, 1924), (1925, 1934), (1953, 2004), (2010, 2039), (2042, 2121), (2128, 2132), (2149, 2230), (2237, 2323), (2328, 2333)]
+'''
+ -1: invalid
+ 0: static or walking
+ 1: MTR
+'''
+
+
+
+d312_20240101 = [(1418, 1427), (1555, 1605), (1852, 1858), (1903, 1907), (1914, 1949), (1957, 2032), (2037, 2041), (2047, 2053), (2133, 2201), (2206, 2233)]
+d312_20240102 = [(1916, 1924), (1925, 1934), (1953, 2004), (2010, 2039), (2042, 2121), (2128, 2132), (2149, 2230), (2237, 2323), (2328, 2333)]
+
+d312_20240308 = []
+d312_20240309 = []
+d312_20240307 = []
+d312_20240306 = []
 
 def save_labelled_data(d, date, serial) -> None:
     filename = f'data\\labelled-{serial}-{date}.json'
@@ -13,9 +26,9 @@ def save_labelled_data(d, date, serial) -> None:
         log_write(f'\nLabelled data successfully saved to {filename}\n')
     except Exception as e:
         log_write(f'\nFailed to store labelled data. Error: {e}\n')
+    return
 
 def time_match(running, record) -> bool:
-    #print(int(record[0:4]))
     global marker
     instant = int(record[0:4])
     for i, (start, end) in enumerate(running):
@@ -33,10 +46,9 @@ def log_write(s) -> None:
         print(s)
     return
         
-def labelling(date, serial) -> None:
-    data = sqllib.loadData(serial, date)
-    running = d612_20240101
-
+def labelling(date, serial, running) -> None:
+    data = sqllib.loadLabelledData(serial, date)
+    
     global marker
     for i in running:
         marker.append(0)
@@ -67,7 +79,7 @@ def removeZero(date, serial):
     data = sqllib.loadLabelledData(serial, date)
     count = 0
     for i in range(len(data)):
-        if int(data[i][2]) == 0:  # SVM_mean = 0 (Invalid)
+        if int(data[i][2]) < 10 or int(data[i][3]) < 10 or int(data[i][4]) < 10:  # SVM_mean = 0 (Invalid)
             data[i][6] = -1
             count += 1
     log_write(f'{count} numbers of invalid (zero) data found.')
@@ -79,26 +91,79 @@ def alignment(date, serial):
         if int(data[i][6]) == 0:
             data[i][6] = -1
 
-    add = 0
     count = 0
 
     for i in range(len(data)):
         if int(data[i][6]) == -1:
             data[i][6] = 0
-            add += data[i][2]
             count += 1
         else:
             break
     for i in range(len(data)):
         if int(data[-i-1][6]) == -1:
             data[-i-1][6] = 0
-            add += data[-i-1][2]
             count += 1
         else:
             break
     save_labelled_data(data, date, serial)
-    log_write(F'{count} numbers of data within transportation turned into invalid.')
-    return add, count
+    log_write(f'{count} numbers of data beyond transportation period turned into blank.')
+    return
+
+def calibration(date, serial, running):
+    data = sqllib.loadLabelledData(serial, date)
+    head = running[0][0]
+    tail = running[-1][-1]
+    headList = range(head-5, head+5)
+    tailList = range(tail-5, tail+5)
+
+    for i in range(len(data)):
+        instant = int(data[i][0][0:4])
+        if instant in headList:
+            left = i - 30
+        elif instant in tailList:
+            right = i + 30
+
+    mean_sum = 0
+    min_sum = 0
+    max_sum = 0
+    cnt = 0
+
+    for i in range(0, left):
+        if data[i][-1] == -1:
+            continue
+        mean_sum += data[i][2]
+        min_sum += data[i][3]
+        max_sum += data[i][4]
+        cnt += 1
+
+    for i in range(right, len(data)):
+        if data[i][-1] == -1:
+            continue
+        mean_sum += data[i][2]
+        min_sum += data[i][3]
+        max_sum += data[i][4]
+        cnt += 1
+    
+    save_labelled_data(data, date, serial)
+    return
+
+def resort(date, serial):
+    data = sqllib.loadData(serial, date)
+    data = sorted(data, key=lambda x: int(x[0]))
+    save_labelled_data(data, date, serial)
+    log_write(f'Data on #{serial} {date} resorted according to time.')
+    return 
+
+def preprocess(date, serial, running):  # standard workflow
+    
+    resort(date, serial)  # sort the data in ascending order
+
+    labelling(date, serial, running)  # tag those data within transportation period
+
+    alignment(date, serial)   # mark out the uesless data within transportation period with value -1
+
+    removeZero(date, serial)  # mark out data with invalid value '0'
+    return
 
 if __name__ == '__main__':
 
@@ -107,22 +172,24 @@ if __name__ == '__main__':
     date = '20240102'
     serial = '312'
     marker = []
+    running = globals()[f'd{serial}_{date}']
 
     log_write(f'------------------\n{time.ctime()}')
 
-    labelling(date, serial)
+    preprocess(date, serial, running)
 
-    removeZero(date, serial)
-
-    a, b = alignment(date, serial)
-    print(a, '\n', b, '\n', a / b)
+    exit(0)
     
 
     
-# data within metro running should be labelled to -1 (invalid) !!!
+# (solved) data within transportation should be labelled to -1 (invalid), in order not to interfere the purity of the training data
+    
+# Mark out those data in static state for calibration
 
 # remove those invalid data
     
 # align the data
         
 # decide the chunk length
+    
+# Kalman filter
